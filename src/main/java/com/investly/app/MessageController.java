be5ef871a274f9baa6f79e8b36e4bd3f1c138653
ivossos <johnvossos2001@gmail.com>
2025-02-04 17:04:22 +0200

    Added Message Controller

--- src/main/java/com/investly/app/MessageController.java
+++ src/main/java/com/investly/app/MessageController.java
@@ -1,10 +1,12 @@
 package com.investly.app;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
@@ -15,5 +17,23 @@ public class MessageController {
 
     private final MessageRepository messageRepository;
     private final MaskRepository maskRepository;
+
+    @PostMapping("/new")
+    public ResponseEntity<MessageEntity> createMessage(@RequestBody MessageEntity messageEntity) {
+        if (messageEntity.getMaskEntity() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "MaskEntity is required");
+        }
+
+        //used valueOf because it needed a string and we provided an int
+        MaskEntity maskEntity = maskRepository.findById(String.valueOf(messageEntity.getMaskEntity().getId()))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Mask entity with this ID not found"));
+
+        messageEntity.setMaskEntity(maskEntity);
+        messageEntity.setTimestamp(OffsetDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(messageRepository.save(messageEntity));
+    }
+
 }
+
 
