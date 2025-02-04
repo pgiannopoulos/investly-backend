8869e820858b96da778bc2fb075525d7a4325ec6
ivossos <johnvossos2001@gmail.com>
2025-02-04 17:17:03 +0200

    Controoler done

--- src/main/java/com/investly/app/MessageController.java
+++ src/main/java/com/investly/app/MessageController.java
@@ -1,8 +1,7 @@
 package com.investly.app;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
