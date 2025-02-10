7f10d54f489aae3f27dbb3799642861d6c47ebb1
ivossos <johnvossos2001@gmail.com>
2025-02-10 19:34:49 +0200

    minor changes

--- src/main/java/com/investly/services/MessageService.java
+++ src/main/java/com/investly/services/MessageService.java
@@ -4,6 +4,7 @@ import com.investly.entities.MaskEntity;
 import com.investly.entities.MessageEntity;
 import com.investly.repositories.MaskRepository;
 import com.investly.repositories.MessageRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,12 @@ import org.springframework.web.server.ResponseStatusException;
 import java.time.OffsetDateTime;
 
 @Service
+@RequiredArgsConstructor
 public class MessageService {
 
     private final MessageRepository messageRepository;
     private final MaskRepository maskRepository;
 
-    @Autowired
-    public MessageService(MessageRepository messageRepository, MaskRepository maskRepository) {
-        this.messageRepository = messageRepository;
-        this.maskRepository = maskRepository;
-    }
 
     public MessageEntity createMessage(Integer maskId, String textPrompt) {
         MaskEntity mask = maskRepository.findById(maskId)
