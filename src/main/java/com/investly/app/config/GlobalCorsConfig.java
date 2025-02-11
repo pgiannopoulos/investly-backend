9278a321673fcdc16677ff6a1d30b0f099bfa4e7
uipko <stikkeruipko@gmail.com>
2025-02-11 22:14:55 +0200

    Websockets work

---
+++ src/main/java/com/investly/app/config/GlobalCorsConfig.java
@@ -0,0 +1,17 @@
+package com.investly.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
