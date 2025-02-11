4c515320ee7952dd732a81201ff80dadf2092d1a
uipko <stikkeruipko@gmail.com>
2025-02-11 17:42:35 +0200

    Websocket Connection Established

---
+++ src/main/java/com/investly/app/controllers/WebSocketController.java
@@ -0,0 +1,23 @@
+package com.investly.app.controllers;
+
+import com.investly.app.dao.MessageEntity;
+import com.investly.app.dto.MessageRequest;
+import com.investly.app.services.MessageService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+    private final MessageService messageService;
+
+    public WebSocketController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+    public MessageEntity processMessage(MessageRequest messageRequest) {
+        return messageService.createMessage(messageRequest.getMaskId(), messageRequest.getTextPrompt());
+    }
+}
