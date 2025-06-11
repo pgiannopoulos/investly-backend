ce6eb98ac363c7de78c28b65c0304e668d3b5fd5
uipko <stikkeruipko@gmail.com>
2025-06-11 14:17:46 +0300

    Created MaskEntity
    # Conflicts:
    #	src/main/java/com/investly/app/MaskEntity.java
    #	src/main/resources/application.properties

---
+++ src/main/java/com/investly/app/MaskEntity.java
@@ -0,0 +1,65 @@
+package com.investly.app;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "masks")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MaskEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String label;
+
+    @OneToMany(mappedBy = "maskEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude  // Prevents infinite recursion in bidirectional relationships
+    private List<MessageEntity> messages;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public List<MessageEntity> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<MessageEntity> messages) {
+        this.messages = messages;
+    }
+}
+
